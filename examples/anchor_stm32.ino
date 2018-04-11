5a355426366906475c069db4d96a4dae2beff118
goktugh <38186466+goktugh@users.noreply.github.com>
2018-04-11 21:12:15 +0300

    Add files via upload

---
+++ examples/anchor_stm32.ino
@@ -0,0 +1,80 @@
+#include <SPI.h>
+#include "DW1000Ranging.h"
+#include "DW1000Device.h"
+
+
+// connection pins
+const uint8_t PIN_RST = PA0; // reset pin
+const uint8_t PIN_IRQ = PA1; // irq pin
+const uint8_t PIN_SS  = PA4; // spi select pin
+const uint8_t LED1  = PB0; // led1
+const uint8_t LED2  = PB1; // led2
+
+
+// active statement
+unsigned long previousMillis = 0; // last time update
+long interval = 10000; // interval at which to do something (milliseconds)
+
+
+  
+
+void setup() {
+  
+
+  Serial2.begin(115200);  
+  
+  delay(1000);
+  
+  //init the configuration
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+ 
+  //define the sketch as anchor. It will be great to dynamically change the type of module
+  DW1000Ranging.attachNewRange(newRange);
+  DW1000Ranging.attachBlinkDevice(newBlink);
+  DW1000Ranging.attachInactiveDevice(inactiveDevice);
+  
+  //we start the module as an anchor
+  DW1000Ranging.startAsAnchor("BA:02:AA:AA:AA:AA:AA:AA", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+
+  // indicate if we are an anchor
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  
+  delay(2000);
+  
+  long int bootime = millis();
+
+ digitalWrite(LED1, HIGH);
+}
+
+void loop() {
+  DW1000Ranging.loop();
+}
+
+void newRange() {
+  // indicate TX
+  digitalWrite(LED2, HIGH);
+
+  // make UDP package
+  unsigned long timestamp = millis();
+  String tagID = DW1000Ranging.getDistantDevice()->getAddress();
+  float tagRange = DW1000Ranging.getDistantDevice()->getRange();
+  float tagPower = DW1000Ranging.getDistantDevice()->getRXPower();
+    
+ 
+  String msg =  timestamp + "," + String(anchorid) + "," + String(tagID) + "," + tagRange + "," + tagPower;
+
+ 
+  Serial2.println(msg);  
+  
+  // indicate TX off
+  digitalWrite(LED2, LOW);
+ }
+
+
+void newBlink(DW1000Device* device) {
+}
+
+void inactiveDevice(DW1000Device* device) {
+}
+
---
+++ examples/tag_stm32.ino
@@ -0,0 +1,79 @@
+#include <SPI.h>
+#include "DW1000Ranging.h"
+#include "DW1000Device.h"
+
+
+// connection pins
+const uint8_t PIN_RST = PA0; // reset pin  // war PB0
+const uint8_t PIN_IRQ = PA1; // irq pin
+const uint8_t PIN_SS = PA4; // spi select pin
+
+const uint8_t LED1  = PB0; // led1
+const uint8_t LED2  = PB1; // led2
+
+
+
+
+  
+void setup() {
+  
+
+  Serial2.begin(115200);
+  
+  // set led outputs
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  
+  delay(1000);
+  
+  //init the configuration
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+  //define the sketch as anchor. It will be great to dynamically change the type of module
+  DW1000Ranging.attachNewRange(newRange);
+  DW1000Ranging.attachNewDevice(newDevice);
+  DW1000Ranging.attachInactiveDevice(inactiveDevice);
+  //Enable the filter to smooth the distance
+  //DW1000Ranging.useRangeFilter(true);
+  
+  //we start the module as a tag
+  DW1000Ranging.startAsTag("AD:14:22:EA:82:60:AF:F9", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+
+  
+
+ }
+
+void loop() {
+  DW1000Ranging.loop();
+}
+
+void newRange() {
+  // print to serial
+  unsigned long timestamp = millis();
+  //Serial2.print("t: "); Serial2.print(timestamp);
+  //Serial2.print("from: "); Serial2.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+  //Serial2.print("\t Range: "); Serial2.print(DW1000Ranging.getDistantDevice()->getRange()); Serial2.print(" m");
+  //Serial2.print("\t RX power: "); Serial2.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial2.println(" dBm");
+
+
+   byte anchorID = DW1000Ranging.getDistantDevice()->getShortAddress();
+    float tagRange = DW1000Ranging.getDistantDevice()->getRange();
+    float tagPower = DW1000Ranging.getDistantDevice()->getRXPower();
+    
+   
+    String msg = timestamp + "," + String(anchorID) +  "," + tagRange + "," + tagPower;
+    Serial2.println(msg);
+  }
+
+
+void newDevice(DW1000Device* device) {
+//  mySerial.print("ranging init; 1 device added ! -> ");
+//  mySerial.print(" short:");
+//  mySerial.println(device->getShortAddress(), HEX);
+}
+
+void inactiveDevice(DW1000Device* device) {
+//  mySerial.print("delete inactive device: ");
+//  mySerial.println(device->getShortAddress(), HEX);
+}
+
+
